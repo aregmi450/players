b3bd683749e67210ef326fa51bdb8b04cc8b73fa
Arun Regmi <aregmi450@gmail.com>
2021-12-24 12:39:29 +0545

    added athelete list for navigation example

--- App.tsx
+++ App.tsx
@@ -1,9 +1,7 @@
-
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {createStackNavigator} from 'react-navigation-stack';
+import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from "react-navigation";
-import { NavigationContainer } from '@react-navigation/native';
 import AthleteProfile from './screens/AthleteProfileScreen';
 import AthleteList from './screens/AthleteListScreen';
 
@@ -25,5 +23,3 @@ export default () => {
     <AthleteList />
   )
 };
-   
-  
