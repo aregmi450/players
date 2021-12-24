52b8d44f223caabc0434ec2b6f44e370e284574a
Arun Regmi <aregmi450@gmail.com>
2021-12-24 15:17:48 +0545

    added on press option on flat list item

--- screens/AthleteListScreen.tsx
+++ screens/AthleteListScreen.tsx
@@ -1,22 +1,34 @@
 import React from "react";
 import {Text,View, FlatList} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import tailwind from "tailwind-rn";
+import { withNavigation } from "react-navigation";
 
 
 const  AthleteList = () => 
     <View style={tailwind('flex-1 pt-10 py-10')}>
     <FlatList
-        data={[
+    showsHorizontalScrollIndicator={false}
+    data={[
             {key: 'Lionel Messi'},
             {key: 'Cristiano Ronaldo'},
             {key: 'Kylian Mbappe'}
         ]}
-        renderItem={({item}) => 
-        <Text style={tailwind('p-2 text-xl h-20 mt-2')}>
-        {item.key}
-        </Text>}
+        renderItem={({item}) => {
+            return (
+                <TouchableOpacity
+                onPress={() => console.log('Hello World')}>
+                    {/* onPress={() => navigation.navigate('AthleteProfile', { id: item.key })}> */}
+                    <Text style={tailwind('p-2 text-xl h-20 mt-2 text-green-600')}>
+                        {item.key}
+                    </Text>
+                </TouchableOpacity> 
+            );
+        }
+    }
         />
+       
 </View>
 
 
-export default AthleteList;
+export default withNavigation(AthleteList);
