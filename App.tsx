8adea6cb37d75a399670d07c2334e824fa1a139f
Arun Regmi <aregmi450@gmail.com>
2021-12-24 16:07:08 +0545

    completed the task

--- App.tsx
+++ App.tsx
@@ -19,23 +19,29 @@
 
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from "react-navigation";
 import AthleteProfile from './screens/AthleteProfileScreen';
 import AthleteList from './screens/AthleteListScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+//move the stack 
+const Stack = createStackNavigator();
+
+ function MainStackNavigator() {
+  return (
+    <Stack.Navigator headerMode={"none"} initialRouteName='AthleteList'>
+      <Stack.Screen name={'AthleteProfile'} component={AthleteProfile} />
+      <Stack.Screen name={'AthleteList'} component={AthleteList} />
+
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return <NavigationContainer>
+    <MainStackNavigator/>
+  </NavigationContainer>
+}
 
 
 
-const navigator = createStackNavigator({
-  AthleteProfile,
-  AthleteList,
-},
-{
-  initialRouteName: 'AthleteList',
-  defaultNavigationOptions: {
-    title: "Athlete List"
-  }
-});
-
-
-export default createAppContainer(navigator);
--- screens/AthleteListScreen.tsx
+++ screens/AthleteListScreen.tsx
@@ -2,33 +2,43 @@ import React from "react";
 import {Text,View, FlatList} from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import tailwind from "tailwind-rn";
-import { withNavigation } from "react-navigation";
+import { useNavigation } from "@react-navigation/native";
+
+type Player = {
+    name: string
+}
+
+const  AthleteList = () => {
+const navigation = useNavigation();
 
 
-const  AthleteList = () => 
-    <View style={tailwind('flex-1 pt-10 py-10')}>
-    <FlatList
-    showsHorizontalScrollIndicator={false}
-    data={[
-            {key: 'Lionel Messi'},
-            {key: 'Cristiano Ronaldo'},
-            {key: 'Kylian Mbappe'}
-        ]}
-        renderItem={({item}) => {
-            return (
-                <TouchableOpacity
-                onPress={() => console.log('Hello World')}>
-                    {/* onPress={() => navigation.navigate('AthleteProfile', { id: item.key })}> */}
-                    <Text style={tailwind('p-2 text-xl h-20 mt-2 text-green-600')}>
-                        {item.key}
-                    </Text>
-                </TouchableOpacity> 
-            );
-        }
-    }
-        />
-       
-</View>
+    return (
+        <View style={tailwind('flex-1 pt-10 py-10')}>
+            <FlatList
+                showsHorizontalScrollIndicator={false}
+                data={[
+                    {
+                        name: 'MESSI'
+                    }
+                ]}
+                renderItem={({ item }) => {
+                    return (
+                        <TouchableOpacity
+                        onPress={() => navigation.navigate('AthleteProfile', {id: item.name})}>
+                            {/* // onPress={() => console.log('Hello World')}> */}
+                            {/* // onPress={() => navigation.navigateAthleteProfile', { id: ('item.key })}> */}
+                            <Text style={tailwind('p-2 text-xl h-20 mt-2 text-green-600')}>
+                                {item.name}
+                            </Text>
+                        </TouchableOpacity>
+                    );
+                }
+            }
+            />
+        </View>
+    )
+}
 
+   
 
-export default withNavigation(AthleteList);
+export default (AthleteList);
